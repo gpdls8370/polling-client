de7ddd1d919a267bf7f94fc40a01e27b1a2f32b6
Hyein <ms8370@naver.com>
2022-10-04 05:56:32 +0900

    작업 폴더 생성
    src 폴더에서 작업하면 됨
    이 뒤로는 각자 브랜치 파서 하기

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        useTabs: false,
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
