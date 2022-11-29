d4f79f96f356964d68664a12a297bf988aa62f32
1105nam <1105nam@naver.com>
2022-11-30 00:52:53 +0900

    공유하기 기능 UX 개선

--- src/components/BalancePost.js
+++ src/components/BalancePost.js
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {Share, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  Share,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {navigation_id, type_color, type_font, url} from './Constants';
 import Icon from 'react-native-vector-icons/EvilIcons';
 import BalancePostBlock from './BalancePostBlock';
@@ -38,16 +45,38 @@ function BalancePost({
       .then(function (data) {
         console.log(data);
 
-        try {
-          const result = Share.share({
-            message:
-              data.socialTitle +
-              '\n' +
-              data.socialDescription +
-              '\n' +
-              data.link,
-          });
+        let result;
 
+        Alert.alert(
+          '공유',
+          '밸런스 공유하기',
+          [
+            {
+              text: '링크만 공유',
+              onPress: async () => {
+                result = Share.share({
+                  message: data.link,
+                });
+              },
+            },
+            {
+              text: '텍스트와 함께 공유',
+              onPress: async () => {
+                result = Share.share({
+                  message:
+                    data.socialTitle +
+                    '\n' +
+                    data.socialDescription +
+                    '\n' +
+                    data.link,
+                });
+              },
+            },
+          ],
+          {cancelable: true},
+        );
+
+        try {
           return result;
         } catch (error) {
           throw new Error('error.message');
